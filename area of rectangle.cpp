aa066a30a82d24b11a82c8859e97893b5afe1068
pujitha076 <animireddypujitha@gmail.com>
2025-06-14 21:18:16 +0530

    Add files via upload
    it is a code on how to find area of a rectangle

---
+++ area of rectangle.cpp
@@ -0,0 +1,11 @@
+//to find area of a rectangle
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int length=2;
+int breadth=3;
+int area=length*breadth;
+printf("area:%d",area);
+	return 0;
+	
+}
