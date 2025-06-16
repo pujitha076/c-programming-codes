db76f81f0ec76aabb9c783a9d4624b9fe36d8e6a
pujitha076 <animireddypujitha@gmail.com>
2025-06-16 22:23:54 +0530

    Add files via upload
    this is a c program code on how to find perimeter of a rectangle

---
+++ perimeter of rectangle.cpp
@@ -0,0 +1,11 @@
+/*to find perimeter of a rectangle*/
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int length=2;
+int breadth=3;
+int perimeter=2*(length+breadth);
+printf("perimeter:%d",perimeter);
+	return 0;
+	
+}
