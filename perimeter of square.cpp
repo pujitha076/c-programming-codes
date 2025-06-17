8d6afa8424ee6739bc1b74e37d20e1199b80e625
pujitha076 <animireddypujitha@gmail.com>
2025-06-17 20:47:02 +0530

    Add files via upload
    this is a c program code on how to find perimeter of square

---
+++ perimeter of square.cpp
@@ -0,0 +1,13 @@
+/*to find perimeter of a square*/
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int side=3;
+int perimeter=4*side;
+printf("perimeter:%d",perimeter);
+	return 0;
+	
+}
+
+
+
