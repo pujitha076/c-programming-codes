172079bae1b9996c5c2f6bae8f2b8450061a640b
pujitha076 <animireddypujitha@gmail.com>
2025-06-18 21:04:04 +0530

    Add files via upload
    this is a c program code on various arithmetic operations

---
+++ arithmatic operations.cpp
@@ -0,0 +1,22 @@
+/*to use various arthematic operations*/
+
+
+#include <stdio.h>
+#include <conio.h>
+int main (){
+	int a=40;
+	int b=20;
+	int sum=a+b;
+	int sub=a-b;
+	int multi=a*b;
+	int div=a/b;
+	printf("sum: %d",sum);
+	printf("\n");
+	printf("sub: %d",sub);
+	printf("\n");
+	printf("multi: %d",multi);
+	printf("\n");
+	printf("div: %d",div);
+	return 0;
+	
+}
