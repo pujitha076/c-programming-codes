922257930b5bd24fa11d2e0ac344d982581d71be
pujitha076 <animireddypujitha@gmail.com>
2025-06-22 21:48:52 +0530

    Add files via upload
    no.of oranges in a farm problem

---
+++ no. of oranges in a farm.cpp
@@ -0,0 +1,12 @@
+/*to calculate oranges */
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int farm=5;
+int trees=80;
+int orange_per_tree=17;
+int orange=farm*trees*orange_per_tree;
+printf("orange:%d",orange);
+	return 0;
+
+}
