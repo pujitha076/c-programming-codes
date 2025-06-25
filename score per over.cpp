9feb215eedf241448b6cadaffac808b199c35960
pujitha076 <animireddypujitha@gmail.com>
2025-06-25 21:02:31 +0530

    Add files via upload
    a problem regarding score per over in cricket

---
+++ score per over.cpp
@@ -0,0 +1,11 @@
+//to calculate score
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int overs=20;
+int score_per_over=6;
+int score=overs*score_per_over;
+printf("score:%d",score);
+	return 0;
+
+}
