d53fe719452075dac0876ae818595ef2a03fa273
pujitha076 <animireddypujitha@gmail.com>
2025-06-27 12:12:09 +0530

    Add files via upload
    this is a code regarding if and else

---
+++ if else example.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("enter a number");
+    scanf("%d",&num);
+    if(num==7)
+    printf("it is 7");
+    else {
+	printf("it is not seven");
+}
+	
+	return 0;
+}
